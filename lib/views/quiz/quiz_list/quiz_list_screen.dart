be0222b9f607dc2aa850d5e54e831b99def36c92
Linh3723 <linh03072003@gmail.com>
2025-11-23 20:17:12 +0700

    Quiz Screen

--- lib/views/quiz/quiz_list/quiz_list_screen.dart
+++ lib/views/quiz/quiz_list/quiz_list_screen.dart
@@ -1,3 +1,6 @@
+import 'package:english_app/core/theme/app_colors.dart';
+import 'package:english_app/services/dummy_data_service.dart';
+import 'package:english_app/views/quiz/quiz_list/widgets/quiz_card.dart';
 import 'package:flutter/material.dart';
 
 class QuizListScreen extends StatelessWidget {
@@ -5,9 +8,59 @@ class QuizListScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Quiz List Screen'),
+      backgroundColor: AppColors.lightBackground,
+      body: CustomScrollView(
+        physics: const BouncingScrollPhysics(),
+        slivers: [
+          SliverAppBar(
+            expandedHeight: 200,
+            pinned: true,
+            backgroundColor: AppColors.primary,
+            flexibleSpace: FlexibleSpaceBar(
+              titlePadding: EdgeInsets.all(16),
+              title: Text(
+                'Quizzes',
+                style: theme.textTheme.headlineMedium?.copyWith(
+                  color: AppColors.accent,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              background: Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [
+                      AppColors.primary,
+                      AppColors.primaryLight,
+                    ],
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          SliverPadding(
+            padding: EdgeInsets.all(20),
+            sliver: SliverList(
+              delegate: SliverChildBuilderDelegate(
+                  (context, index){
+                    final quiz = DummyDataService.quizzes[index];
+                    return QuizCard(
+                      title: quiz.title,
+                      description: quiz.description,
+                      questionCount: quiz.questions.length,
+                      timeLimit: quiz.timeLimit,
+                      onTap: () {},
+                    );
+                  },
+                childCount: DummyDataService.quizzes.length,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
